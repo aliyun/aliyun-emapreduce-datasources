512b8a6ebf53fd155815c1d9fde4785fe9b98888
uncleGen <hustyugm@gmail.com>
2017-01-11 15:06:29 +0800

    bug fix

--- external/emr-logservice/src/main/scala/org/apache/spark/streaming/aliyun/logservice/DirectLoghubInputDStream.scala
+++ external/emr-logservice/src/main/scala/org/apache/spark/streaming/aliyun/logservice/DirectLoghubInputDStream.scala
@@ -40,6 +40,7 @@ import org.apache.spark.streaming.scheduler.StreamInputInfo
 import org.apache.spark.util.Utils
 
 import scala.collection.mutable.{ArrayBuffer, HashMap}
+import scala.collection.JavaConversions._
 
 class DirectLoghubInputDStream(
     _ssc: StreamingContext,
@@ -64,11 +65,16 @@ class DirectLoghubInputDStream(
   private var doCommit: Boolean = false
   @transient private var restartTime: Long = -1L
   @transient private var restart: Boolean = false
+  private var lastJobTime: Long = -1L
 
   override def start(): Unit = {
     val props = new Properties()
     zkParams.foreach(param => props.put(param._1, param._2))
     val autoCommit = zkParams.getOrElse("enable.auto.commit", "false").toBoolean
+    // TODO: support concurrent jobs
+    val concurrentJobs = ssc.conf.getInt(s"spark.streaming.concurrentJobs", 1)
+    require(concurrentJobs == 1, "Loghub direct api only supports running one job at each time, " +
+      "but \"spark.streaming.concurrentJobs\"=" + concurrentJobs)
     require(StringUtils.isNotEmpty(ssc.checkpointDir) && !autoCommit, "Disable auto commit by " +
       "setting \"enable.auto.commit=false\" and enable checkpoint.")
     checkpointDir = new Path(ssc.checkpointDir).toUri.getPath
@@ -124,37 +130,25 @@ class DirectLoghubInputDStream(
     }
     val diff = mClient.ListShard(project, logStore).GetShards().map(_.GetShardId()).diff(initial)
     diff.foreach(shardId => {
-      var checkPoints: util.ArrayList[ConsumerGroupShardCheckPoint] = null
-      try {
-        checkPoints = mClient.GetCheckPoint(project, logStore, mConsumerGroup, shardId)
-          .GetCheckPoints()
-      } finally {
-        // Do nothing.
-      }
-      val checkpoint = if (CollectionUtils.isEmpty(checkPoints)) {
-        logWarning(s"Can not find any checkpoint for specific consumer group $mConsumerGroup")
-        null
-      } else {
-        checkPoints.get(0).getCheckPoint
-      }
-
-      val nextCursor = if (StringUtils.isNoneEmpty(checkpoint)) {
-        checkpoint
-      } else {
-        val cursor = mode match {
-          case LogHubCursorPosition.END_CURSOR =>
-            mClient.GetCursor(project, logStore, shardId, CursorMode.END)
-          case LogHubCursorPosition.BEGIN_CURSOR =>
-            mClient.GetCursor(project, logStore, shardId, CursorMode.BEGIN)
-          case LogHubCursorPosition.SPECIAL_TIMER_CURSOR =>
-            mClient.GetCursor(project, logStore, shardId, cursorStartTime)
-        }
-        cursor.GetCursor()
-      }
-
+      val nextCursor = fetchCursorFromLoghub(shardId)
       DirectLoghubInputDStream.writeDataToZK(zkClient, s"$checkpointDir/consume/$shardId.shard",
         nextCursor)
     })
+
+    // Clean commit data in zookeeper in case of restarting streaming job but lose checkpoint file
+    // in `checkpointDir`
+    import scala.collection.JavaConversions._
+    try {
+      zkClient.getChildren(s"$checkpointDir/commit").foreach(child => {
+        zkClient.delete(s"$checkpointDir/commit/$child")
+      })
+      doCommit = false
+    } catch {
+      case e: ZkNoNodeException =>
+        logDebug("If this is the first time to run, it is fine to not find any commit data in " +
+          "zookeeper.")
+        // Do nothing, make compiler happy.
+    }
   }
 
   override def stop(): Unit = {
@@ -176,21 +170,48 @@ class DirectLoghubInputDStream(
 
     COMMIT_LOCK.synchronized {
       val shardOffsets = new ArrayBuffer[(Int, String, String)]()
-      val rdd = if (doCommit && validTime.milliseconds > restartTime) {
-        if (restart) {
-          // At the first time after restart, we should recompute from the last `consume` offset.
+      val lastFailed: Boolean = {
+        if(doCommit) {
+          false
+        } else {
+          val pendingTimes = ssc.scheduler.getPendingTimes()
+          val pending = pendingTimes.exists(time => time.milliseconds == lastJobTime)
+          if (pending) {
+            false
+          } else {
+            true
+          }
+        }
+      }
+      val rdd = if (validTime.milliseconds > restartTime && (doCommit || lastFailed)) {
+        if (restart || lastFailed) {
+          // 1. At the first time after restart, we should recompute from the last `consume`
+          // offset. (TODO: confirm this logic?)
+          // 2. If last batch job failed, we should also recompute from last `consume` offset.
           // Then, set `restart=false` to continue committing.
           restart = false
         } else {
           commitAll()
         }
-        import scala.collection.JavaConversions._
-        mClient.ListShard(project, logStore).GetShards().foreach(shard => {
-          val shardId = shard.GetShardId()
-          val start: String = zkClient.readData(s"$checkpointDir/consume/$shardId.shard")
-          val end = mClient.GetCursor(project, logStore, shardId, CursorMode.END).GetCursor()
-          shardOffsets.+=((shardId, start, end))
-        })
+        try {
+          mClient.ListShard(project, logStore).GetShards().foreach(shard => {
+            val shardId = shard.GetShardId()
+            val start: String = zkClient.readData(s"$checkpointDir/consume/$shardId.shard")
+            val end = mClient.GetCursor(project, logStore, shardId, CursorMode.END).GetCursor()
+            shardOffsets.+=((shardId, start, end))
+          })
+        } catch {
+          case e: ZkNoNodeException =>
+            logWarning("Loghub consuming metadata was lost in zookeeper, refetch from loghub " +
+              "checkpoint")
+            mClient.ListShard(project, logStore).GetShards().foreach(shard => {
+              val shardId = shard.GetShardId()
+              val start: String = fetchCursorFromLoghub(shardId)
+              val end = mClient.GetCursor(project, logStore, shardId, CursorMode.END).GetCursor()
+              shardOffsets.+=((shardId, start, end))
+            })
+        }
+        lastJobTime = validTime.milliseconds
         new LoghubRDD(
           ssc.sc,
           project,
@@ -267,6 +288,7 @@ class DirectLoghubInputDStream(
 
   @throws(classOf[IOException])
   private def readObject(ois: ObjectInputStream): Unit = Utils.tryOrIOException {
+    // TODO: move following logic to proper restart code path.
     this.synchronized {
       logDebug(s"${this.getClass().getSimpleName}.readObject used")
       ois.defaultReadObject()
@@ -315,7 +337,7 @@ class DirectLoghubInputDStream(
           try {
             val consumerGroups = mClient.ListConsumerGroup(project, logStore).GetConsumerGroups()
             import scala.collection.JavaConversions._
-            consumerGroups.filter(cg => cg.getConsumerGroupName.equals(mConsumerGroup)).size match {
+            consumerGroups.count(cg => cg.getConsumerGroupName.equals(mConsumerGroup)) match {
               case 1 =>
                 logInfo("Create consumer group successfully.")
               case 0 =>
@@ -331,6 +353,38 @@ class DirectLoghubInputDStream(
             "errorCode: " + e.GetErrorCode() + ", errorMessage: " + e.GetErrorMessage())
         }
     }
+  }
+
+  private def fetchCursorFromLoghub(shardId: Int): String = {
+    var checkPoints: util.ArrayList[ConsumerGroupShardCheckPoint] = null
+    try {
+      checkPoints = mClient.GetCheckPoint(project, logStore, mConsumerGroup, shardId)
+        .GetCheckPoints()
+    } finally {
+      // Do nothing.
+    }
+    val checkpoint = if (CollectionUtils.isEmpty(checkPoints)) {
+      logWarning(s"Can not find any checkpoint for specific consumer group $mConsumerGroup")
+      null
+    } else {
+      checkPoints.get(0).getCheckPoint
+    }
+
+    val nextCursor = if (StringUtils.isNoneEmpty(checkpoint)) {
+      checkpoint
+    } else {
+      val cursor = mode match {
+        case LogHubCursorPosition.END_CURSOR =>
+          mClient.GetCursor(project, logStore, shardId, CursorMode.END)
+        case LogHubCursorPosition.BEGIN_CURSOR =>
+          mClient.GetCursor(project, logStore, shardId, CursorMode.BEGIN)
+        case LogHubCursorPosition.SPECIAL_TIMER_CURSOR =>
+          mClient.GetCursor(project, logStore, shardId, cursorStartTime)
+      }
+      cursor.GetCursor()
+    }
+
+    nextCursor
   }
 }
 
