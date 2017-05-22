2f2d149ec09b874feea5c222948f80d4d403be6b
windpiger <songjun@outlook.com>
2017-05-22 14:56:50 +0800

    change DateType to TimeStampType, and fix a npe bug (#181)
    fix odps python date format,add HH:mm:ss

--- external/emr-maxcompute/src/main/scala/org/apache/spark/aliyun/odps/PythonOdpsAPI.scala
+++ external/emr-maxcompute/src/main/scala/org/apache/spark/aliyun/odps/PythonOdpsAPI.scala
@@ -43,7 +43,7 @@ class PythonOdpsAPI(
       odpsUrl: String,
       tunnelUrl: String) extends Logging with Serializable {
 
-  val dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+  val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
   val odpsOps = OdpsOps(jsc.sc, accessKeyId, accessKeySecret, odpsUrl, tunnelUrl)
 
   def readTable(
@@ -126,7 +126,11 @@ class PythonOdpsAPI(
         case OdpsType.BIGINT => record.getBigint(idx)
         case OdpsType.DOUBLE => record.getDouble(idx)
         case OdpsType.BOOLEAN => record.getBoolean(idx)
-        case OdpsType.DATETIME => dateFormat.format(record.getDatetime(idx))
+        case OdpsType.DATETIME =>
+          val dt = record.getDatetime(idx)
+          if (dt != null) {
+            dateFormat.format(record.getDatetime(idx))
+          } else null
         case OdpsType.STRING => if(isBytes == 1) record.getBytes(idx) else record.getString(idx)
       }
     }
