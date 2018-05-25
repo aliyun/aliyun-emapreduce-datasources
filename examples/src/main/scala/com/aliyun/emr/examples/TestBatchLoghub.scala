81768ba09ec6036649a550e7d085b7699c4697f8
wenxuanguan <choose_home@126.com>
2018-05-25 14:16:22 +0800

    add spark batch log service rdd (#211)
     add spark batch log service rdd

---
+++ examples/src/main/scala/com/aliyun/emr/examples/TestBatchLoghub.scala
@@ -0,0 +1,51 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.aliyun.emr.examples
+
+import org.apache.spark.api.java.JavaRDD
+import org.apache.spark.streaming.aliyun.logservice.LoghubUtils
+import org.apache.spark.{SparkConf, SparkContext}
+
+object TestBatchLoghub {
+  def main(args: Array[String]): Unit = {
+    if (args.length < 6) {
+      System.err.println(
+        """Usage: TestBatchLoghub <sls project> <sls logstore> <sls endpoint>
+          |         <access key id> <access key secret> <start time> <end time=now>
+        """.stripMargin)
+      System.exit(1)
+    }
+
+    val loghubProject = args(0)
+    val logStore = args(1)
+    val endpoint = args(2)
+    val accessKeyId = args(3)
+    val accessKeySecret = args(4)
+    val startTime = args(5).toLong
+
+    val sc = new SparkContext(new SparkConf().setAppName("test batch loghub"))
+    var rdd:JavaRDD[String] = null
+    if (args.length > 6) {
+      rdd = LoghubUtils.createRDD(sc, loghubProject, logStore, accessKeyId, accessKeySecret, endpoint, startTime, args(6).toLong)
+    } else {
+      rdd = LoghubUtils.createRDD(sc, loghubProject, logStore, accessKeyId, accessKeySecret, endpoint, startTime)
+    }
+
+    println("get log count:" + rdd.count())
+
+  }
+}
