fd835bc28f186b3b7c410aef453bd06f3b6d3f85
wenxuanguan <choose_home@126.com>
2019-02-19 15:41:54 +0800

    fix OnsUtils createDefaultStreams lost message body

--- emr-ons/src/main/scala/org/apache/spark/streaming/aliyun/ons/OnsUtils.scala
+++ emr-ons/src/main/scala/org/apache/spark/streaming/aliyun/ons/OnsUtils.scala
@@ -16,6 +16,8 @@
  */
 package org.apache.spark.streaming.aliyun.ons
 
+import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.serializer.SerializeFilter
 import com.aliyun.openservices.ons.api.Message
 import org.apache.spark.annotation.Experimental
 import org.apache.spark.api.java.function.{Function => JFunction}
@@ -186,7 +188,9 @@ object OnsUtils {
       (msg: Message) => func.call(msg))
   }
 
-  private def extractMessage(msg: Message): Array[Byte] = msg.toString.getBytes
+  private def extractMessage(msg: Message): Array[Byte] = {
+    JSON.toJSONString(msg, new SerializeFilter{}).getBytes()
+  }
 
   @Experimental
   def createDefaultStreams(
