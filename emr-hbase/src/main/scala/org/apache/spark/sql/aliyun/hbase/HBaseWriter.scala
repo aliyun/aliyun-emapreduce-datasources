68ea7f104bba49c58c83bf3f5543914f6f623edf
uncleGen <hustyugm@gmail.com>
2019-06-18 12:21:14 +0800

    update pom

--- emr-hbase/src/main/scala/org/apache/spark/sql/aliyun/hbase/HBaseWriter.scala
+++ emr-hbase/src/main/scala/org/apache/spark/sql/aliyun/hbase/HBaseWriter.scala
@@ -30,7 +30,6 @@ class HBaseWriter(parameters: Map[String, String]) {
       sourceOptions: Map[String, String]): Unit = {
     val schema = StructType.fromAttributes(queryExecution.analyzed.output)
     val relation = HBaseRelation(parameters, Some(schema))(sparkSession.sqlContext)
-    relation.createTableIfNotExist()
     val encoder = RowEncoder(schema).resolveAndBind()
     val rdd = queryExecution.toRdd.map(r => encoder.fromRow(r))
     relation.insert(sparkSession.createDataFrame(rdd, schema), false)
