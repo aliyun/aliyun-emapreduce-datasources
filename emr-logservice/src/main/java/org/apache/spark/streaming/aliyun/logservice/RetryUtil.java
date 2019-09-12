a52778347ac002b2d0f544c8dfbc5cfb6cf41cf1
liketic <ke.like@outlook.com>
2019-09-12 13:09:15 +0800

    incr retries

--- emr-logservice/src/main/java/org/apache/spark/streaming/aliyun/logservice/RetryUtil.java
+++ emr-logservice/src/main/java/org/apache/spark/streaming/aliyun/logservice/RetryUtil.java
@@ -28,7 +28,7 @@ final class RetryUtil {
 
   private static final long maxBackoff = 10000;
   private static final long initialBackoff = 1000;
-  private static final int maxRetry = 6;
+  private static final int maxRetry = 10;
 
   static <T> T call(Callable<T> callable) throws Exception {
     long backoff = initialBackoff;
