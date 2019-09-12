cfe3fdea196240430565c91afac8aa2180afaec1
liketic <ke.like@outlook.com>
2019-09-12 13:06:46 +0800

    fix indents

--- emr-logservice/src/main/java/org/apache/spark/streaming/aliyun/logservice/RetryUtil.java
+++ emr-logservice/src/main/java/org/apache/spark/streaming/aliyun/logservice/RetryUtil.java
@@ -24,38 +24,38 @@ import org.apache.commons.logging.LogFactory;
 import java.util.concurrent.Callable;
 
 final class RetryUtil {
-    private static final Log LOG = LogFactory.getLog(RetryUtil.class);
+  private static final Log LOG = LogFactory.getLog(RetryUtil.class);
 
-    private static final long maxBackoff = 10000;
-    private static final long initialBackoff = 1000;
-    private static final int maxRetry = 6;
+  private static final long maxBackoff = 10000;
+  private static final long initialBackoff = 1000;
+  private static final int maxRetry = 6;
 
-    static <T> T call(Callable<T> callable) throws Exception {
-        long backoff = initialBackoff;
-        int retries = 0;
-        do {
-            try {
-                return callable.call();
-            } catch (LogException ex) {
-                if (ex.GetHttpCode() < 500) {
-                    if (retries >= maxRetry) {
-                        LOG.error("reconnect to log-service exceed max retry times[" + maxRetry + "].");
-                        throw ex;
-                    }
-                    ++retries;
-                } else {
-                    // always retry on internal server error
-                    retries = 0;
-                }
-            } catch (Exception ex) {
-                if (retries >= maxRetry) {
-                    LOG.error("reconnect to log-service exceed max retry times[" + maxRetry + "].");
-                    throw ex;
-                }
-                ++retries;
-            }
-            Thread.sleep(backoff);
-            backoff = Math.min(backoff * 2, maxBackoff);
-        } while (true);
-    }
+  static <T> T call(Callable<T> callable) throws Exception {
+    long backoff = initialBackoff;
+    int retries = 0;
+    do {
+      try {
+        return callable.call();
+      } catch (LogException ex) {
+        if (ex.GetHttpCode() < 500) {
+          if (retries >= maxRetry) {
+            LOG.error("reconnect to log-service exceed max retry times[" + maxRetry + "].");
+            throw ex;
+          }
+          ++retries;
+        } else {
+          // always retry on internal server error
+          retries = 0;
+        }
+      } catch (Exception ex) {
+        if (retries >= maxRetry) {
+          LOG.error("reconnect to log-service exceed max retry times[" + maxRetry + "].");
+          throw ex;
+        }
+        ++retries;
+      }
+      Thread.sleep(backoff);
+      backoff = Math.min(backoff * 2, maxBackoff);
+    } while (true);
+  }
 }
