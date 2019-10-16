24f1143f01280cc5d6c285a3f9b3958319d1e716
Like <ke.like@outlook.com>
2019-10-16 14:36:30 +0800

    Separate checkpoint dir and no need to fetch checkpoint for every shard (#376)

--- emr-logservice/src/main/scala/org/apache/spark/streaming/aliyun/logservice/DirectLoghubInputDStream.scala
+++ emr-logservice/src/main/scala/org/apache/spark/streaming/aliyun/logservice/DirectLoghubInputDStream.scala
@@ -16,47 +16,45 @@
  */
 package org.apache.spark.streaming.aliyun.logservice
 
-import java.io.{IOException, ObjectInputStream, UnsupportedEncodingException}
-import java.util
-import java.util.Properties
+import java.io.{IOException, ObjectInputStream}
+import java.nio.charset.StandardCharsets
 
 import com.aliyun.openservices.log.common.Consts.CursorMode
-import com.aliyun.openservices.log.common.{ConsumerGroup, ConsumerGroupShardCheckPoint}
+import com.aliyun.openservices.log.common.ConsumerGroup
 import com.aliyun.openservices.log.exception.LogException
 import com.aliyun.openservices.loghub.client.config.LogHubCursorPosition
 import com.aliyun.openservices.loghub.client.exceptions.LogHubClientWorkerException
 import org.I0Itec.zkclient.ZkClient
 import org.I0Itec.zkclient.exception.ZkNoNodeException
 import org.I0Itec.zkclient.serialize.ZkSerializer
-import org.apache.commons.collections.CollectionUtils
 import org.apache.commons.lang3.StringUtils
 import org.apache.hadoop.fs.Path
 import org.apache.spark.internal.Logging
 import org.apache.spark.rdd.RDD
 import org.apache.spark.storage.StorageLevel
-import org.apache.spark.streaming.{StreamingContext, Time}
 import org.apache.spark.streaming.dstream.{DStreamCheckpointData, InputDStream}
 import org.apache.spark.streaming.scheduler.StreamInputInfo
+import org.apache.spark.streaming.{StreamingContext, Time}
 import org.apache.spark.util.Utils
 
-import scala.collection.mutable.{ArrayBuffer, HashMap}
 import scala.collection.JavaConversions._
 import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 
 class DirectLoghubInputDStream(
     _ssc: StreamingContext,
     project: String,
     logStore: String,
-    mConsumerGroup: String,
+    consumerGroup: String,
     accessKeyId: String,
     accessKeySecret: String,
     endpoint: String,
     zkParams: Map[String, String],
     mode: LogHubCursorPosition,
     cursorStartTime: Long = -1L
-  ) extends InputDStream[String](_ssc) with Logging with CanCommitOffsets {
+) extends InputDStream[String](_ssc) with Logging with CanCommitOffsets {
   @transient private var zkClient: ZkClient = null
-  @transient private var mClient: LoghubClientAgent = null
+  @transient private var loghubClient: LoghubClientAgent = null
   @transient private var COMMIT_LOCK = new Object()
   private val zkConnect = zkParams.getOrElse("zookeeper.connect", "localhost:2181")
   private val zkSessionTimeoutMs = zkParams.getOrElse("zookeeper.session.timeout.ms", "6000").toInt
@@ -78,46 +76,22 @@ class DirectLoghubInputDStream(
         s"total process time in each batch. You can disable the behavior by setting " +
         s"'spark.streaming.loghub.count.precise.enable'=false")
     } else {
-      logWarning(s"Disable precise count on loghub rdd, we will get an approximative count of loghub rdd, " +
+      logWarning(s"Disable precise count on loghub rdd, we will get an approximate count of loghub rdd, " +
         s"via the `GetHistograms` api of log service. You can enable the behavior by setting " +
         s"'spark.streaming.loghub.count.precise.enable'=true")
     }
-
-    val props = new Properties()
-    zkParams.foreach(param => props.put(param._1, param._2))
-    val autoCommit = zkParams.getOrElse("enable.auto.commit", "false").toBoolean
     // TODO: support concurrent jobs
     val concurrentJobs = ssc.conf.getInt(s"spark.streaming.concurrentJobs", 1)
     require(concurrentJobs == 1, "Loghub direct api only supports one job concurrently, " +
       "but \"spark.streaming.concurrentJobs\"=" + concurrentJobs)
-    require(StringUtils.isNotEmpty(ssc.checkpointDir) && !autoCommit, "Disable auto commit by " +
-      "setting \"enable.auto.commit=false\" and enable checkpoint.")
-    checkpointDir = new Path(ssc.checkpointDir).toUri.getPath
-    if (!autoCommit) {
-      zkClient = new ZkClient(zkConnect, zkSessionTimeoutMs, zkConnectionTimeoutMs)
-      zkClient.setZkSerializer(new ZkSerializer() {
-        override def serialize(data: scala.Any): Array[Byte] = {
-          try {
-            data.asInstanceOf[String].getBytes("UTF-8")
-          } catch {
-            case _: UnsupportedEncodingException =>
-              null
-          }
-        }
 
-        override def deserialize(bytes: Array[Byte]): AnyRef = {
-          if (bytes == null) {
-            return null
-          }
-          try {
-            new String(bytes, "UTF-8")
-          } catch {
-            case _: UnsupportedEncodingException =>
-              null
-          }
-        }
-      })
+    var zkCheckpointDir = ssc.checkpointDir
+    if (StringUtils.isBlank(zkCheckpointDir)) {
+      zkCheckpointDir = s"/$consumerGroup"
+      logInfo(s"Checkpoint dir was not specified, using consumer group $consumerGroup as checkpoint dir")
     }
+    checkpointDir = new Path(zkCheckpointDir).toUri.getPath
+    createZkClient()
 
     try {
       // Check if zookeeper is usable. Direct loghub api depends on zookeeper.
@@ -131,8 +105,7 @@ class DirectLoghubInputDStream(
         throw new RuntimeException("Loghub direct api depends on zookeeper. Make sure that " +
           "zookeeper is on active service.", e)
     }
-
-    mClient = new LoghubClientAgent(endpoint, accessKeyId, accessKeySecret)
+    loghubClient = new LoghubClientAgent(endpoint, accessKeyId, accessKeySecret)
 
     tryToCreateConsumerGroup()
 
@@ -143,12 +116,14 @@ class DirectLoghubInputDStream(
     } else {
       Array.empty[String]
     }
-    val diff = mClient.ListShard(project, logStore).GetShards().map(_.GetShardId()).diff(initial)
-    diff.foreach(shardId => {
-      val nextCursor = fetchCursorFromLoghub(shardId)
-      DirectLoghubInputDStream.writeDataToZK(zkClient, s"$checkpointDir/consume/$project/$logStore/$shardId.shard",
-        nextCursor)
-    })
+    val diff = loghubClient.ListShard(project, logStore).GetShards().map(_.GetShardId()).diff(initial)
+    if (diff.nonEmpty) {
+      val checkpoints = fetchAllCheckpoints()
+      diff.foreach(shardId => {
+        val cursor = findCheckpointOrCursorForShard(shardId, checkpoints)
+        DirectLoghubInputDStream.writeDataToZK(zkClient, s"$checkpointDir/consume/$project/$logStore/$shardId.shard", cursor)
+      })
+    }
 
     // Clean commit data in zookeeper in case of restarting streaming job but lose checkpoint file
     // in `checkpointDir`
@@ -162,7 +137,6 @@ class DirectLoghubInputDStream(
       case _: ZkNoNodeException =>
         logDebug("If this is the first time to run, it is fine to not find any commit data in " +
           "zookeeper.")
-        // Do nothing, make compiler happy.
     }
   }
 
@@ -188,7 +162,7 @@ class DirectLoghubInputDStream(
     COMMIT_LOCK.synchronized {
       val shardOffsets = new ArrayBuffer[(Int, String, String)]()
       val lastFailed: Boolean = {
-        if(doCommit) {
+        if (doCommit) {
           false
         } else {
           val pendingTimes = ssc.scheduler.getPendingTimes()
@@ -211,19 +185,19 @@ class DirectLoghubInputDStream(
           commitAll()
         }
         try {
-          mClient.ListShard(project, logStore).GetShards().foreach(shard => {
+          loghubClient.ListShard(project, logStore).GetShards().foreach(shard => {
             val shardId = shard.GetShardId()
-            if (shard.getStatus.toLowerCase.equals("readonly") && readOnlyShardCache.contains(shardId)) {
-              // do nothing
+            val isReadonly = shard.getStatus.equalsIgnoreCase("readonly")
+            if (isReadonly && readOnlyShardCache.contains(shardId)) {
               logDebug(s"There is no data to consume from shard $shardId.")
             } else {
               val start: String = zkClient.readData(s"$checkpointDir/consume/$project/$logStore/$shardId.shard")
-              val end = mClient.GetCursor(project, logStore, shardId, CursorMode.END).GetCursor()
+              val end = loghubClient.GetCursor(project, logStore, shardId, CursorMode.END).GetCursor()
               logInfo(s"ShardID $shardId, start $start end $end")
               if (!start.equals(end)) {
                 shardOffsets.+=((shardId, start, end))
               }
-              if (start.equals(end) && shard.getStatus.toLowerCase.equals("readonly")) {
+              if (start.equals(end) && isReadonly) {
                 readOnlyShardCache.put(shardId, end)
               }
             }
@@ -232,10 +206,11 @@ class DirectLoghubInputDStream(
           case _: ZkNoNodeException =>
             logWarning("Loghub consuming metadata was lost in zookeeper, re-fetch from loghub " +
               "checkpoint")
-            mClient.ListShard(project, logStore).GetShards().foreach(shard => {
+            val checkpoints = fetchAllCheckpoints()
+            loghubClient.ListShard(project, logStore).GetShards().foreach(shard => {
               val shardId = shard.GetShardId()
-              val start: String = fetchCursorFromLoghub(shardId)
-              val end = mClient.GetCursor(project, logStore, shardId, CursorMode.END).GetCursor()
+              val start: String = findCheckpointOrCursorForShard(shardId, checkpoints)
+              val end = loghubClient.GetCursor(project, logStore, shardId, CursorMode.END).GetCursor()
               logInfo(s"ShardID $shardId, start $start end $end")
               shardOffsets.+=((shardId, start, end))
             })
@@ -294,8 +269,8 @@ class DirectLoghubInputDStream(
           val shardId = child.substring(0, child.indexOf(".")).toInt
           if (!readOnlyShardCache.contains(shardId)) {
             val data: String = zkClient.readData(s"$checkpointDir/commit/$project/$logStore/$child")
-            log.info(s"Updating checkpoint $data of shard $shardId to consumer group $mConsumerGroup")
-            mClient.UpdateCheckPoint(project, logStore, mConsumerGroup, shardId, data)
+            log.debug(s"Updating checkpoint $data of shard $shardId to consumer group $consumerGroup")
+            loghubClient.UpdateCheckPoint(project, logStore, consumerGroup, shardId, data)
             DirectLoghubInputDStream.writeDataToZK(zkClient, s"$checkpointDir/consume/$project/$logStore/$child", data)
           }
         })
@@ -317,53 +292,45 @@ class DirectLoghubInputDStream(
     this.synchronized {
       logDebug(s"${this.getClass.getSimpleName}.readObject used")
       ois.defaultReadObject()
-      generatedRDDs = new HashMap[Time, RDD[String]]()
+      generatedRDDs = new mutable.HashMap[Time, RDD[String]]()
       readOnlyShardCache = new mutable.HashMap[Int, String]()
       COMMIT_LOCK = new Object()
-      val autoCommit = zkParams.getOrElse("enable.auto.commit", "true").toBoolean
-      require(checkpointDir.nonEmpty || autoCommit, "Enable auto commit by setting " +
-        "\"enable.auto.commit=true\" or enable checkpoint.")
-      if (!autoCommit) {
-        zkClient = new ZkClient(zkConnect, zkSessionTimeoutMs, zkConnectionTimeoutMs)
-        zkClient.setZkSerializer(new ZkSerializer() {
-          override def serialize(data: scala.Any): Array[Byte] = {
-            try {
-              data.asInstanceOf[String].getBytes("UTF-8")
-            } catch {
-              case e: UnsupportedEncodingException =>
-                null
-            }
-          }
-
-          override def deserialize(bytes: Array[Byte]): AnyRef = {
-            if (bytes == null) {
-              return null
-            }
-            try {
-              new String(bytes, "UTF-8")
-            } catch {
-              case _: UnsupportedEncodingException =>
-                null
-            }
-          }
-        })
-      }
-      mClient = new LoghubClientAgent(endpoint, accessKeyId, accessKeySecret)
-      startTime = -1L
-      restart = true
+      createZkClient()
     }
+    loghubClient = new LoghubClientAgent(endpoint, accessKeyId, accessKeySecret)
+    startTime = -1L
+    restart = true
+  }
+
+  private def createZkClient(): Unit = {
+    zkClient = new ZkClient(zkConnect, zkSessionTimeoutMs, zkConnectionTimeoutMs)
+    zkClient.setZkSerializer(new ZkSerializer() {
+      override def serialize(data: scala.Any): Array[Byte] = {
+        if (data == null) {
+          return null
+        }
+        data.asInstanceOf[String].getBytes(StandardCharsets.UTF_8)
+      }
+
+      override def deserialize(bytes: Array[Byte]): AnyRef = {
+        if (bytes == null) {
+          return null
+        }
+        new String(bytes, StandardCharsets.UTF_8)
+      }
+    })
   }
 
   private def tryToCreateConsumerGroup(): Unit = {
     try {
-      mClient.CreateConsumerGroup(project, logStore, new ConsumerGroup(mConsumerGroup, 10, true))
+      loghubClient.CreateConsumerGroup(project, logStore, new ConsumerGroup(consumerGroup, 10, true))
     } catch {
       case e: LogException =>
         if (e.GetErrorCode.compareToIgnoreCase("ConsumerGroupAlreadyExist") == 0) {
           try {
-            val consumerGroups = mClient.ListConsumerGroup(project, logStore).GetConsumerGroups()
+            val consumerGroups = loghubClient.ListConsumerGroup(project, logStore).GetConsumerGroups()
             import scala.collection.JavaConversions._
-            consumerGroups.count(cg => cg.getConsumerGroupName.equals(mConsumerGroup)) match {
+            consumerGroups.count(cg => cg.getConsumerGroupName.equals(consumerGroup)) match {
               case 1 =>
                 logInfo("Create consumer group successfully.")
               case 0 =>
@@ -381,36 +348,37 @@ class DirectLoghubInputDStream(
     }
   }
 
-  private def fetchCursorFromLoghub(shardId: Int): String = {
-    var checkPoints: util.ArrayList[ConsumerGroupShardCheckPoint] = null
+  private def fetchAllCheckpoints(): mutable.Map[Int, String] = {
+    val checkpoints = new mutable.HashMap[Int, String]()
     try {
-      checkPoints = mClient.GetCheckPoint(project, logStore, mConsumerGroup, shardId)
-        .GetCheckPoints()
-    } finally {
-      // Do nothing.
-    }
-    val checkpoint = if (CollectionUtils.isEmpty(checkPoints)) {
-      logWarning(s"Can not find any checkpoint for specific consumer group $mConsumerGroup")
-      null
-    } else {
-      checkPoints.get(0).getCheckPoint
-    }
-
-    val nextCursor = if (StringUtils.isNoneEmpty(checkpoint)) {
-      checkpoint
-    } else {
-      val cursor = mode match {
-        case LogHubCursorPosition.END_CURSOR =>
-          mClient.GetCursor(project, logStore, shardId, CursorMode.END)
-        case LogHubCursorPosition.BEGIN_CURSOR =>
-          mClient.GetCursor(project, logStore, shardId, CursorMode.BEGIN)
-        case LogHubCursorPosition.SPECIAL_TIMER_CURSOR =>
-          mClient.GetCursor(project, logStore, shardId, cursorStartTime)
+      val result = loghubClient.ListCheckpoints(project, logStore, consumerGroup)
+      if (result != null) {
+        result.getCheckPoints.foreach(item => {
+          checkpoints.put(item.getShard, item.getCheckPoint)
+        })
       }
-      cursor.GetCursor()
+    } catch {
+      case e: Exception =>
+        throw new RuntimeException("Cannot fetch checkpoint from remote server", e)
     }
+    checkpoints
+  }
 
-    nextCursor
+  private def findCheckpointOrCursorForShard(shardId: Int, checkpoints: mutable.Map[Int, String]): String = {
+    val checkpoint = checkpoints.getOrElse(shardId, null)
+    if (checkpoint != null) {
+      logInfo(s"Shard $shardId will start from checkpoint $checkpoint")
+      return checkpoint
+    }
+    val cursor = mode match {
+      case LogHubCursorPosition.END_CURSOR =>
+        loghubClient.GetCursor(project, logStore, shardId, CursorMode.END)
+      case LogHubCursorPosition.BEGIN_CURSOR =>
+        loghubClient.GetCursor(project, logStore, shardId, CursorMode.BEGIN)
+      case LogHubCursorPosition.SPECIAL_TIMER_CURSOR =>
+        loghubClient.GetCursor(project, logStore, shardId, cursorStartTime)
+    }
+    cursor.GetCursor()
   }
 
   private class DirectLoghubInputDStreamCheckpointData extends DStreamCheckpointData(this) {
